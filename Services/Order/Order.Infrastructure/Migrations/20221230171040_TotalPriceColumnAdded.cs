37c3c787726a12d59fb5c055a0fdff6ef1022412
Turan Öz <turan.oz@armagroupholding.com>
2023-01-04 12:06:27 +0300

    migration initial

---
+++ Services/Order/Order.Infrastructure/Migrations/20221230171040_TotalPriceColumnAdded.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Order.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TotalPriceColumnAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "GetTotalPrice",
+                schema: "ordering",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GetTotalPrice",
+                schema: "ordering",
+                table: "Orders");
+        }
+    }
+}
---
+++ Services/Order/Order.Infrastructure/Migrations/20221230171159_tt.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Order.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class tt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GetTotalPrice",
+                schema: "ordering",
+                table: "Orders",
+                newName: "TotalPrice");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalPrice",
+                schema: "ordering",
+                table: "Orders",
+                newName: "GetTotalPrice");
+        }
+    }
+}
